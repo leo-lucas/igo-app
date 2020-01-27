19d5fa12b39ffcdfca03a415c7f109e2882e16da
Leonardo Lucas dos Santos <leonardo.santos@meta.com.br>
2020-01-27 02:25:57 +0000

    add provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React, { FC } from 'react'
 import { Provider } from 'react-redux'
 
 import { store } from './Store'
+import { Text } from 'react-native'
 
 const App: FC = () => {
   return (
     <Provider store={store}>
-      <App />
+      <Text>IGO</Text>
     </Provider>
   )
 }
